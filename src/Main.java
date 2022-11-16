1a0b2fe1d32f0dc950f25c3aa2dce9f8490f3d56
BT_3W2_20 <dariyab@nycstudents.net>
2022-11-16 09:31:52 -0500

    halfway

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args)
+    {
+
+
+        for (int j=4; j>0;j--)
+        {
+            for (int i = j; i > 0; i--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i=0;i<6;i++)
+
+
+
+
+
+    }
 }
+
